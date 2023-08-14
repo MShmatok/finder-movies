6c20596b357584ab3e8399b25c6f3fd2a000e2c4
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-08-14 12:56:33 +0300

    fix lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,35 @@
-import Layouts from 'layouts/Layouts';
-import Home from 'pages/home/Home';
-import Movies from 'pages/movies/Movies';
-import MoviesDetails from 'pages/moviesDetails/MoviesDetails';
+// import Layouts from 'layouts/Layouts';
+// import Home from 'pages/home/Home';
+// import Movies from 'pages/movies/Movies';
+// import MoviesDetails from 'pages/moviesDetails/MoviesDetails';
+// import Cast from './Cast/Cast';
+// import Reviews from './Reviews/Reviews';
 
 import { Route, Routes } from 'react-router-dom';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
+import { Suspense, lazy } from 'react';
+
+const Layouts = lazy(() => import('layouts/Layouts'));
+const Home = lazy(() => import('pages/home/Home'));
+const Movies = lazy(() => import('pages/movies/Movies'));
+const MoviesDetails = lazy(() => import('pages/moviesDetails/MoviesDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layouts />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MoviesDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Layouts />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MoviesDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </>
   );
 };
